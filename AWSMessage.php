76edce563fbe729a55496c835189b8c569728025
Spyros Soldatos <spyros.soldatos@peopleperhour.com>
2012-11-09 10:24:03 +0200

    Added attributes sentTimestamp senderId on message receive

--- AWSMessage.php
+++ AWSMessage.php
@@ -20,10 +20,20 @@ class AWSMessage extends CModel
      * @var string md5 of the message body
      */
     public $md5;
+    /**
+     * @var int Timestamp when the message was created
+     */
+    public $sentTimestamp;
+
+    /**
+     * @var string The id of the sender
+     */
+    public $senderId;
+
 
     public function attributeNames()
     {
-        return array('body','id','receiptHandle','md5');
+        return array('body','id','receiptHandle','md5','sentTimestamp','senderId');
     }
 
     public function toString()
